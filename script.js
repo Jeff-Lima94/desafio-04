51af04d05cb67d623ad77a934be59405621ccd87
Jeff-Lima94 <jeffersonlima10.jls@gmail.com>
2022-08-03 14:23:45 -0300

    firt commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function OláMundo (){
+   console.log("Olá Mundo")
+ }
+ 
+ OláMundo ()
+ 
+ 
+ function helloFriend (name){
+   console.log("O nome é: " + name)
+ 
+ }
+ 
+ helloFriend ("Jefferson")
+ 
+ 
+ function Dados (name,idade,estiloMusica){
+   console.log(`Olá ${name},sua idade é ${idade} e vc gosta de ${estiloMusica}`)
+ 
+ }
+ 
+Dados ("Jefferson","28","Rock")
+
+function Movie_Music (Filme,Musica){
+
+  console.log(`Seu filme preferido é ${Filme} e sua música ${Musica}`)
+}
+
+Movie_Music ("Harry Potter","Living on a Prayer")
+
+
+
